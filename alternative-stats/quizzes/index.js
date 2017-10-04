868bf8f3ac975f20661e2747efa13d7e9b666118
coalman <tehcoalman@gmail.com>
2017-10-04 14:23:23 -0400

    fixed quiz list ordering

--- alternative-stats/quizzes/index.js
+++ alternative-stats/quizzes/index.js
@@ -4,7 +4,7 @@ import {iterToLookup} from 'alternative-stats/utils';
 const quizRequire = require.context('./', false, /\.yaml$/);
 const quizList = quizRequire.keys()
       .map((quizFile) => quizRequire(quizFile))
-      .sort((a, b) => (new Date(a.publishdate) - new Date(b.publishdate)));
+      .sort((a, b) => (new Date(b.publishdate) - new Date(a.publishdate)));
 
 export function getQuizId(quiz) {
   return slugify(quiz.name, {lower: true});
